9a134ff1a025f3612881d7175bbd386b55cb1adf
Niels Gregersen <nielshtg@gmail.com>
2019-11-27 09:01:16 +0100

    WIP

--- app/Startup.cs
+++ app/Startup.cs
@@ -70,6 +70,14 @@ namespace app
                 options.Filters.Add(typeof(HttpGlobalExceptionFilter));
             });
 
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                options.HttpsPort = 443;
+            });
+
+            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

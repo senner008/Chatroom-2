2929786658895fba0d82841d8074ce19009545a7
Niels Gregersen <nielshtg@gmail.com>
2019-11-29 03:36:06 +0100

    WIP

--- app/src/render/render-rooms.ts
+++ app/src/render/render-rooms.ts
@@ -6,17 +6,17 @@ export const RoomRender = (() =>  {
     const _createLi =  room => `<li data-id='${room.id}' class="list-group-item">${room.name}</li>`;
 
     const self  = {
-            append : room => $(_roomsList).append(_createLi(room)),
-            exists : id => _findById(id).length > 0,
-            renderList : (rooms) => {
-                if (rooms.length === 0) return;
-                $(_roomsList).html(rooms.map(_createLi));
-            },
-            renderSelect : id => {
-                $(_roomsList).find('li').removeClass("room-selected");
-                _findById(id).addClass("room-selected");
-            }
+        append : room => $(_roomsList).append(_createLi(room)),
+        exists : id => _findById(id).length > 0,
+        renderList : (rooms) => {
+            if (rooms.length === 0) return;
+            $(_roomsList).html(rooms.map(_createLi));
+        },
+        renderSelect : id => {
+            $(_roomsList).find('li').removeClass("room-selected");
+            _findById(id).addClass("room-selected");
         }
+    }
     return self;
 })();
 
